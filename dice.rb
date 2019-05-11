1ddd730ff339aaf3d4454fbf4dd6d25fcfe70843
Yang Liu <jeremyrobturtle@gmail.com>
2019-05-11 04:49:34 -0700

    DicePool

--- dice.rb
+++ dice.rb
@@ -13,3 +13,23 @@ class Dice
     @random.rand(@range)
   end
 end
+
+class DicePool
+  attr_reader :results
+
+  def initialize(capacity, dice: Dice.new)
+    @capacity = capacity
+    @results = Array.new(capacity) { dice.roll }
+  end
+
+  # @return [void] Yield the result if present, otherwise do nothing.
+  def use(index)
+    raise 'Invalid index of dice pool' if index < 0 || index >= @capacity
+    raise 'The result already been taken' if @results[index].nil?
+
+    if (result = @results[index])
+      yield result
+      @results[index] = nil
+    end
+  end
+end
