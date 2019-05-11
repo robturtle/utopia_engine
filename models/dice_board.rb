1105aabea44bde5e0147c56e53a49e9f84eca703
Yang Liu <jeremyrobturtle@gmail.com>
2019-05-11 12:42:16 -0700

    fix style

--- models/dice_board.rb
+++ models/dice_board.rb
@@ -3,13 +3,14 @@
 # The class template of a rectangle area which can be filled with
 # dice values.
 class DiceBoard
+  # rubocop:disable all
   # @param rows [Integer]
   # @param cols [Integer]
   def self.new(rows, cols)
-    raise 'rows must > 0' unless rows > 0
-    raise 'cols must > 0' unless cols > 0
+    raise 'rows must > 0' unless rows.positive?
+    raise 'cols must > 0' unless cols.positive?
 
-    board_class = Class.new do
+    clazz = Class.new do
       # @return [Array<Array<Integer>>] values of the board
       attr_reader :slots
 
@@ -21,13 +22,16 @@ class DiceBoard
       # @param col [Integer] 0 <= col < cols
       # @param value [Integer] the dice value
       def fill(row, col, value)
-        raise "Index out of bound! (row: #{row}, col: #{col})" unless row >= 0 && row < rows && col >= 0 && col < cols
+        unless row >= 0 && row < rows && col >= 0 && col < cols
+          raise "Index out of bound! (row: #{row}, col: #{col})"
+        end
         raise 'Slot already filled!' if @slots[row][col]
 
         slots[row][col] = value
       end
 
-      # @return [Array<Tuple<Integer,Integer>>] The list of (row,col) with empty slot.
+      # @return [Array<Tuple<Integer,Integer>>]
+      #   The list of (row,col) with empty slot.
       def emptys
         [*0...rows].product([*0...cols]).select { |r, c| slots[r][c].nil? }
       end
@@ -54,7 +58,9 @@ class DiceBoard
       # @param idx [Integer] The 1-dimentional coordindate
       # @param value [Integer] the dice value
       def fill_idx(idx, value)
-        raise "Index out fo bound! (idx: #{idx})" unless idx >= 0 && idx < slot_size
+        unless idx >= 0 && idx < slot_size
+          raise "Index out fo bound! (idx: #{idx})"
+        end
 
         fill(*from_idx(idx), value)
       end
@@ -64,9 +70,10 @@ class DiceBoard
         emptys.map { |r, c| idx(r, c) }
       end
     end
-    board_class.class_eval "def rows; #{rows}; end"
-    board_class.class_eval "def cols; #{cols}; end"
-    board_class.class_eval "def slot_size; #{rows * cols}; end"
-    board_class
+    clazz.class_eval "def rows; #{rows}; end", __FILE__, __LINE__
+    clazz.class_eval "def cols; #{cols}; end", __FILE__, __LINE__
+    clazz.class_eval "def slot_size; #{rows * cols}; end", __FILE__, __LINE__
+    clazz
   end
+  # rubocop:enable all
 end
