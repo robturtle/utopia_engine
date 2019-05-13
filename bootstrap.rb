6f0cafb8f24b3ba4d6fda0e6b160def06698bb62
Yang Liu <jeremyrobturtle@gmail.com>
2019-05-12 20:10:26 -0700

    hack require_all

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'core_ext'
-require_all 'views'
 require_all 'models'
 require_all 'actions'
+# TODO: use require_all gem and not require CLI related files here
+require_all 'cli/views'
